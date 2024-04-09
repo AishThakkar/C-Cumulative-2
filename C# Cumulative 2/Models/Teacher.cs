198b48a49e19ed361d22539be1d3c002720f732c
AishThakkar <aishthakkar@gmail.com>
2024-04-08 23:14:38 -0400

    add

---
+++ C# Cumulative 2/Models/Teacher.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace C__Cumulative_2.Models
+{
+    public class Teacher
+    {
+        //This properties define an Teacher
+ 
+        public DateTime HireDate;
+        public int TeacherId;
+        public string TeacherFname;
+        public string TeacherLname;
+        public string EmployeeNumber;
+        public decimal Salary;
+
+        public Teacher()
+        {
+
+        }
+    }
+}
